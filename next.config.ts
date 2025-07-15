d4b2bcf646c5da4940f4ee5b2e19fd4a512d315e
KevinPhuc <kevinphuc.work@gmail.com>
2025-07-15 15:21:47 +0700

    Update for github pages build

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  distDir: 'dist',
   images: {
     domains: ['images.unsplash.com'],
+    unoptimized: true,
   },
   devIndicators: false,
 };
